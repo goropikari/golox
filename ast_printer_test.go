245edb588c970fb7014447a9760c1c7b473eb755
goropikari <goropikari56@gmail.com>
2021-09-05 13:01:42 +0900

    add ast_printer test

--- ast_printer_test.go
+++ ast_printer_test.go
@@ -1,17 +1,34 @@
 package tlps_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/goropikari/tlps"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAstPrinter(t *testing.T) {
-	expression := tlps.NewBinary(
-		tlps.NewUnary(tlps.NewToken(tlps.Minus, "-", nil, 1), tlps.NewLiteral(123)),
-		tlps.NewToken(tlps.Star, "*", nil, 1),
-		tlps.NewGrouping(tlps.NewLiteral(45.67)),
-	)
-	fmt.Println(tlps.NewAstPrinter().Print(expression))
+	ast := tlps.NewAstPrinter()
+
+	var tests = []struct {
+		name     string
+		expected string
+		given    tlps.Expr
+	}{
+		{
+			name:     "",
+			expected: "(* (- 123) (group 45.67))",
+			given: tlps.NewBinary(
+				tlps.NewUnary(tlps.NewToken(tlps.Minus, "-", nil, 1), tlps.NewLiteral(123)),
+				tlps.NewToken(tlps.Star, "*", nil, 1),
+				tlps.NewGrouping(tlps.NewLiteral(45.67))),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			actual, _ := ast.Print(tt.given)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
 }
